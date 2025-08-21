fc959b995ea142042575c8bcea393fde0d7b59a5
Levi Bostian <levi@levi.earth>
2025-08-21 06:50:34 -0500

    ci: setup deployment, including publishing to jsr

---
+++ scripts/deployment-deploy.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env -S deno run --quiet --allow-all
+
+import $ from "jsr:@david/dax";
+import { getDeployStepInput } from "../main.ts"
+
+const input = getDeployStepInput()
+
+// Deno publish 
+
+const argsToDenoPublish = [
+  "publish",
+  "--set-version",
+  input.nextVersionName
+]
+
+if (input.testMode) {
+  argsToDenoPublish.push("--dry-run")
+}
+
+// https://github.com/dsherret/dax#providing-arguments-to-a-command
+await $`deno ${argsToDenoPublish}`.printCommand()
+
+// GitHub Release 
+
+const argsToCreateGithubRelease = [
+  `release`,
+  `create`,
+  input.nextVersionName,
+  `--generate-notes`,
+  `--latest`,
+  `--target`,
+  'main'
+]
+
+if (input.testMode) {
+  console.log("Running in test mode, skipping creating GitHub release.")
+  console.log(`Command to create GitHub release: gh ${argsToCreateGithubRelease.join(" ")}`)
+
+  Deno.exit(0)
+}
+
+await $`gh ${argsToCreateGithubRelease}`.printCommand()
+
---
+++ scripts/deployment-get-latest-release.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S deno run --quiet --allow-all
+
+import { getLatestReleaseStepInput, setLatestReleaseStepOutput } from "../main.ts"
+import $ from "jsr:@david/dax";
+
+const input = getLatestReleaseStepInput()
+
+const latestReleaseJsonText = await $`gh release list --exclude-drafts --order desc --json name,isLatest,isPrerelease,tagName --jq '.[0]'`.text()
+
+if (!latestReleaseJsonText) {
+  // no releases found. meaning we have not yet made a release. 
+  // exit early without setting output. decaf will make a first time release. 
+  Deno.exit(0) 
+}
+const latestRelease: {
+  name: string,
+  tagName: string
+} = JSON.parse(latestReleaseJsonText)
+
+const commitMatchingRelease = input.gitCommitsCurrentBranch.find((commit) => {
+  return commit.tags?.includes(latestRelease.tagName)
+})!
+
+setLatestReleaseStepOutput({
+  versionName: latestRelease.name,
+  commitSha: commitMatchingRelease.sha,
+})
